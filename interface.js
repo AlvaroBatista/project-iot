b12c4e26876848492fe01a7646cb55c8f6b1849d
Alvaro Batista <alvinho.jesus@hotmail.com>
2021-05-26 20:33:56 -0300

    recebendo dados do firebase

--- interface.js
+++ interface.js
@@ -4,7 +4,61 @@
 //     firebaseRef.set(2);
 //     console.log(1);
 // });
-firebase.database().ref('sala/distancia').on('value', snapshot =>{
-    let l = snapshot.val();
-    console.log('>>', l)
+
+
+/** Recebendo dados do firebase */
+firebase.database().ref('sala/distancia').on('value', snapshot => {
+    var distancia = '';
+    distancia = snapshot.val();
+    console.log('distancia >>', distancia)
+    document.getElementById('distanciaValue').innerText = distancia;
+});
+
+firebase.database().ref('sala/temperatura').on('value', snapshot => {
+    var temperatura = '';
+    temperatura = snapshot.val();
+    console.log('temperatura >>', temperatura)
+    document.getElementById('temperaturaValue').innerText = temperatura + ' C°';
+});
+
+firebase.database().ref('sala/luminosidade').on('value', snapshot => {
+    var luminosidade = '';
+    luminosidade = snapshot.val();
+    console.log('luminosidade >>', luminosidade)
+    document.getElementById('luminosidadeValue').innerText = luminosidade;
+});
+
+firebase.database().ref('sala/umidade').on('value', snapshot => {
+    var umidade = '';
+    umidade = snapshot.val();
+    console.log('umidade >>', umidade)
+    document.getElementById('umidadeValue').innerText = umidade;
+});
+
+firebase.database().ref('sala/porta').on('value', snapshot => {
+    let porta = document.querySelector('#porta');
+    var portaValue = '';
+    portaValue = snapshot.val();
+    console.log('porta >>', portaValue);
+    if (portaValue == '1') {
+        porta.src = 'porta_fechada.jpg';
+        document.getElementById('porta-title').innerText = 'Porta Fechada';
+    } else {
+        porta.src = 'porta_aberta.jpg';
+        document.getElementById('porta-title').innerText = 'Porta Aberta';
+    }
+});
+
+firebase.database().ref('sala/presenca').on('value', snapshot => {
+    let presenca = document.querySelector('#presenca');
+    var presencaValue = '';
+    presencaValue = snapshot.val();
+    console.log('presenca >>', presencaValue);
+    if (presencaValue == '1') {
+        presenca.src = 'presenca_detectada.jpg';
+        document.getElementById('presenca-title').innerText = 'Presença Detectada';
+    } else {
+        presenca.src = 'presenca_nao_detectada.jpg';
+        document.getElementById('presenca-title').innerText = 'Presença Não Detectada';
+    }
 });
